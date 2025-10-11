6590114c0894b0b3dd68e79adf4fbce3c4fc8cf2
kamil-j-ambi <kamil@cut2code.com>
2025-10-11 16:28:41 +0200

    FEAT: menu and db export.

--- web/app/mu-plugins/aios-firewall-loader.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Begin AIOWPSEC Firewall
-if (file_exists('D:/wamp64/www/bedrock/web/aios-bootstrap.php')) {
-	include_once('D:/wamp64/www/bedrock/web/aios-bootstrap.php');
-}
-// End AIOWPSEC Firewall
